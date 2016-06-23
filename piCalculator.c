52ecce3d105034cb4a52bb199103eb0c425b593c
AdamD2 <adam_douglas2@hotmail.com>
2016-06-23 22:16:54 +1000

    This commit was mainly based on style and documentation. - Created a README file. - Added the executable into the git repository. - Defined some names for each of the methods to make the menu code more readable. - Added comments throughout the code.

--- piCalculator.c
+++ piCalculator.c
@@ -16,6 +16,7 @@ int main (int argc, char *argv[]) {
     printf ("==========================================\n\n");
     
     do {
+        // Give choices for Pi methods and get user input
         printf ("Methods: \n\n");
         printf ("1 - Lebiniz\n");
         printf ("2 - Bailey-Borwein-Plouffle (Faulty)\n");
@@ -24,26 +25,29 @@ int main (int argc, char *argv[]) {
         
         printf ("Which would you like to calculate? ");
         scanf ("%d", &menuChoice);
-        while (menuChoice < 1 && menuChoice > 4) {
+        // Check that the input is valid
+        while (menuChoice < LEIBNIZ && menuChoice > GAUSS) {
             printf ("Incorrect input. Choose from either 1, 2, 3 or 4.");
             printf ("Which would you like to calculate? ");
             scanf ("%d", &menuChoice);
         }       
             
+        // Call the relevant functions based on user input
         switch (menuChoice) {
-            case 1: lebinizPi ();
+            case LEIBNIZ: lebinizPi ();
             break;
-            case 2: bbpPi ();
+            case BBP: bbpPi ();
             break;
-            case 3: rsPi ();
+            case RAMANUJAN: rsPi ();
             break;
-            case 4: glPi ();
+            case GAUSS: glPi ();
             break;
             default: printf ("An input error occurred. Please try again.");
             break;
         }
         printf ("\n");
         
+        // Ask user if they want to repeat the program and check input
         printf ("Would you like to repeat the program? Y/N");
         scanf ("%c", &tempLoop);
         while (tempLoop != 'Y' && tempLoop != 'N') {
@@ -52,6 +56,7 @@ int main (int argc, char *argv[]) {
             scanf ("%c", &tempLoop);
         }
         
+        // Assign the correct boolean value based on user input
         if (tempLoop == 'Y') {
             loop = TRUE;
         } else {
@@ -71,9 +76,12 @@ void lebinizPi () {
     
     printf ("Calculating...");
     
+    // Apply the formula for the number of iterations specified, with 
+    // the original formula being 1/4*Pi = 1-1/3+1/5-1/7+1/9 and so on
     for (int i = 0; i < iterations; i++) {
         pi = pi + pow(-1, i) / (2*i + 1);
     }
+    // Multiply 4 to get the final result
     pi = 4*pi;
     
     printPi (pi);
@@ -86,6 +94,7 @@ void bbpPi () {
     printf ("\nThis method calculates up to 256 digits of pi.");
     iterations = iterationQuestion (256);
     
+    // Apply the formula
     for (int k = 0; k < iterations; k++) {
         pi = (4 / (8*k+1) - 2 / (8*k+4) - 1 / (8*k+5) - 1 / (8*k+6));
         pi = pi * pow(16, -k);
@@ -113,12 +122,15 @@ void rsPi () {
 }
 
 void glPi () {
+    // Declare all of the variables used in the formula
     long double pi = 0, a = 1, b = 1/sqrt(2), t = 0.25, p = 1, tempA;
     int iterations; 
     
     printf("\nThis method doubles in accuracy for each iteration.");
     iterations = iterationQuestion(999999999);
     
+    // Assign the variables to each individual part of the formula
+    // iteratively
     for(int i = 0; i <= iterations; i++) {
         tempA = a;
         a = (tempA + b) / 2;
@@ -151,6 +163,7 @@ int iterationQuestion (int limit)
 int factorial (int a) {
     int d = 1;
     
+    // Basic factorial function
     for (int b = 1; b <= a; b++) {
         d *= b;
     }
